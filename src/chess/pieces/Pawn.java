83e825d9ba4e7548985eca72096d09b7a5a52e73
Alexandre Brito <alexandrybrito@gmail.com>
2023-11-17 15:35:36 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,18 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
+
 public class Pawn extends ChessPiece {
+	
+	private ChessMatch chessMatch;
 
 	public Pawn(Board board, Color color) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 	
 	
@@ -53,7 +58,7 @@ public class Pawn extends ChessPiece {
 			}
 			
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getColumn() + 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
